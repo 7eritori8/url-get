0c1134d5b62d96221f3425309f7eea0a6fcae297
eritori <7eritori8@gmail.com>
2020-04-11 14:51:02 +0900

    説明文を変更

--- resources/views/urlget.blade.php
+++ resources/views/urlget.blade.php
@@ -1,12 +1,18 @@
 {{csrf_field()}}
+
 <form method="get">
-    <p>検索するURL</p>
-    <input type="text" name="url">
+    <p>検索するURLを入力してください</p>
+    <input type="text" name="url"size="20">
     <p>
-        <input type="submit" value="URLを入力してタイトルを取得">
+        <input type="submit" value="タイトルを取得">
+    </p>
+    <p>
+        <a href="/">リセットする</a>
     </p>
 </form>
-
+<h2>このサイトの説明</h2>
+<p>このサイトはURLを上のフォームに入力すると、そのサイトの過去１０年分のタイトルを取得できます。</p>
+<p>※しばらく時間がかかります。気長にしばらくお待ち下さい。修正予定です。</p>
 <?php
 
 if (isset($_GET['url'])) {
@@ -21,12 +27,14 @@ if (isset($_GET['url'])) {
 //    入力されたURLにhttps://web.archive.orgを合体
 //    $past_urlにarchive入のURLを代入
         $past_url = "https://web.archive.org/web/" . $url_year."/". $url;
-        echo $past_url . "<br>";
+//        echo $url_year. "年<br>";
         $souce = file_get_contents($past_url);
         if(preg_match('/<title>(.*?)<\/title>/i',$souce,$result)==1){
             echo '<textarea cols="40" rows="3">';
             echo $result[1];
-            echo'</textarea>';
+            echo'</textarea><br>';
+            echo '<br>';
+//            echo $past_url;
 
         }
     }
